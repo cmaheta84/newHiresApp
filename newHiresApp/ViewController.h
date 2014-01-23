c28230eb5120ddb6e78d4e60f98e9ac6251bcf1a
Chandni Maheta <chandni@shoulderhair-lm.corp.yahoo.com>
2014-01-23 14:21:48 -0800

    Initial Commit

---
+++ newHiresApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newHiresApp
+//
+//  Created by Chandni Maheta on 1/23/14.
+//  Copyright (c) 2014 Chandni Maheta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ newHiresAppTests/newHiresAppTests.m
@@ -0,0 +1,34 @@
+//
+//  newHiresAppTests.m
+//  newHiresAppTests
+//
+//  Created by Chandni Maheta on 1/23/14.
+//  Copyright (c) 2014 Chandni Maheta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface newHiresAppTests : XCTestCase
+
+@end
+
+@implementation newHiresAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
