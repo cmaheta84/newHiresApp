767bad87aaad9d496a1cbf37ee050a923b1d2a7b
Chandni Maheta <chandni@shoulderhair-lm.corp.yahoo.com>
2014-01-31 12:23:04 -0800

    3

---
+++ newHiresApp/SocialDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialDetailsViewController.h
+//  newHiresApp
+//
+//  Created by Chandni Maheta on 1/31/14.
+//  Copyright (c) 2014 Chandni Maheta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialDetailsViewController : UIViewController
+
+@end
---
+++ newHiresApp/SocialDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SocialDetailsViewController.m
+//  newHiresApp
+//
+//  Created by Chandni Maheta on 1/31/14.
+//  Copyright (c) 2014 Chandni Maheta. All rights reserved.
+//
+
+#import "SocialDetailsViewController.h"
+
+@interface SocialDetailsViewController ()
+
+@end
+
+@implementation SocialDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
